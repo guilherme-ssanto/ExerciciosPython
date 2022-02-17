35c3046eb1fdf845574fd7276bd544342eb0d8f8
Guilherme <guilhermesantossilva@id.uff.br>
2022-02-16 21:29:04 -0300

    Create Desafio07.py

---
+++ Desafio07.py
@@ -0,0 +1,4 @@
+n1 = float(input("Digite a nota do Aluno na prova 1: "))
+n2 = float(input("Digite a nota do Aluno na prova 2: "))
+n3 = float(input("Digite a nota do aluno na prova 3: "))
+print("A média do Aluno é de {:.1f}".format((n1+n2+n3)/3))
