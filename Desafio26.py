77012760830a145f782eaef07af9c82e11310920
Guilherme <guilhermesantossilva@id.uff.br>
2022-02-16 21:29:56 -0300

    Create Desafio26.py

---
+++ Desafio26.py
@@ -0,0 +1,5 @@
+frase = str(input("Escreva uma frase: ")).strip()
+frase = frase.upper()
+print("A letra A aparece {} vezes na frase.".format(frase.count("A")))
+print("A letra A apareceu na posição {}".format(frase.find("A")+1))
+print("A letra A apareceu na posição {}".format(frase.rfind("A")+1))
